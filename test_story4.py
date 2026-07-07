091331a5249d6eb305ded5cae58c2cbaaaa0e2a5
yanxin-bsu <yan.xin@bsu.edu>
2026-07-07 11:07:39 +0800

    change story4

--- test_story4.py
+++ test_story4.py
@@ -1,7 +1,9 @@
-from story4 import is_time_conflict, has_time_conflict
+from story4 import is_time_conflict, find_valid_schedule
 
 
 def test_same_day_time_conflict():
+    """Test overlapping time on same days."""
+
     course1 = {
         "days": "MWF",
         "start": 900,
@@ -17,7 +19,10 @@ def test_same_day_time_conflict():
     assert is_time_conflict(course1, course2) is True
 
 
+
 def test_different_days_no_conflict():
+    """Test different meeting days."""
+
     course1 = {
         "days": "MWF",
         "start": 900,
@@ -33,7 +38,10 @@ def test_different_days_no_conflict():
     assert is_time_conflict(course1, course2) is False
 
 
+
 def test_same_day_no_time_overlap():
+    """Test same days but different time."""
+
     course1 = {
         "days": "MWF",
         "start": 900,
@@ -49,7 +57,13 @@ def test_same_day_no_time_overlap():
     assert is_time_conflict(course1, course2) is False
 
 
-def test_multiple_sections_conflict(tmp_path):
+
+def test_find_valid_schedule_with_alternative_section(tmp_path):
+    """
+    Test system can find another section
+    when the first section conflicts.
+    """
+
     course_file = tmp_path / "courses.txt"
 
     course_file.write_text(
@@ -60,21 +74,49 @@ def test_multiple_sections_conflict(tmp_path):
 
     files = [str(course_file)]
 
-    selected_courses = ["CS222", "CS120"]
+    selected_courses = [
+        "CS222",
+        "CS120"
+    ]
 
-    assert has_time_conflict(selected_courses, files) is True
+    result = find_valid_schedule(
+        selected_courses,
+        files
+    )
+
+    assert result is not None
+
+    assert result[0]["course"] == "CS222"
+    assert result[0]["section"] == "002"
+
+    assert result[1]["course"] == "CS120"
 
 
-def test_multiple_sections_no_conflict(tmp_path):
+
+def test_no_valid_schedule(tmp_path):
+    """
+    Test when all sections have conflicts.
+    """
+
     course_file = tmp_path / "courses.txt"
 
     course_file.write_text(
-        "CS222 001 TR 900 1000\n"
+        "CS222 001 MWF 900 1400\n"
+        "CS222 002 MWF 900 1400\n"
         "CS120 001 MWF 930 1030\n"
+        "CS120 002 MWF 1130 1230\n"
     )
 
     files = [str(course_file)]
 
-    selected_courses = ["CS222", "CS120"]
+    selected_courses = [
+        "CS222",
+        "CS120"
+    ]
 
-    assert has_time_conflict(selected_courses, files) is False
+    result = find_valid_schedule(
+        selected_courses,
+        files
+    )
+
+    assert result is None
